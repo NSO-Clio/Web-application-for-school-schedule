6707432c11fefc944bd33da17642469af6d3e3a9
EgorAndrik <egorandreas28@gmail.com>
2023-12-04 09:04:23 +0700

    creat API

---
+++ api/CreatTables.py
@@ -0,0 +1,100 @@
+import gspread
+from gspread import Client, Spreadsheet, Worksheet
+import pandas as pd
+
+
+def get_all_values_in_ws(ws: Worksheet) -> list:
+    list_of_lists = ws.get_all_values()
+    return list_of_lists
+
+
+class TableWorker:
+    def __init__(
+            self, url: str, id_gid: list, id_gid_consult: list,
+            path_service_account: str
+    ):
+        self._SPREADSHEET_UPL = url
+        self._id_gid = id_gid
+        self._id_gid_consult = id_gid_consult
+
+        self._path_service_account = path_service_account
+        self._gc: Client = gspread.service_account(self._path_service_account)
+        self._sh: Spreadsheet = self._gc.open_by_url(self._SPREADSHEET_UPL)
+
+        self.DataFrames = {}
+        self.DataClasses = {}
+
+    def creatData(self):
+        tmp_data = [get_all_values_in_ws(self._sh.get_worksheet_by_id(i)) for i in self._id_gid]
+        data_df = [pd.DataFrame(data=elem[1:], columns=elem[0]).rename(columns={
+            'Дни': 'Дни' + str(i), 'Уроки': 'Уроки' + str(i), 'Время': 'Время' + str(i)
+        }) for i, elem in enumerate(tmp_data)]
+        result = pd.concat(data_df, axis=1)
+        result = result[result.columns[:3].to_list() +
+                        [i for i in result.iloc[:, 3:].columns if i not in result.columns[:3].to_list()]]
+        result = result.rename(columns={i: i.lower().replace(' ', '').replace('+', '') for i in result.columns[3:]})
+        result = result.rename(columns={'Дни0': 'Дни', 'Уроки0': 'Уроки', 'Время0': 'Время'})
+        result['Дни'] = (result['Дни'].map(lambda x: x.lower()).replace('понедельник', 'ПН').replace('вторник', 'ВТ')
+                         .replace('среда', 'СР').replace('четверг', 'ЧТ').replace('пятница', 'ПТ')
+                         .replace('суббота', 'СБ'))
+        self.DataFrames['gid'] = result
+
+        tmp_data = [get_all_values_in_ws(self._sh.get_worksheet_by_id(i)) for i in self._id_gid_consult]
+        tmp_data = [pd.DataFrame(data=i[2:], columns=i[1]).rename(columns={'': 'Учителя'}) for i in tmp_data]
+        data_consult = pd.concat(tmp_data, axis=0).reset_index().iloc[:, 1:]
+
+        days_list = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ']
+        k = 0
+        for i in range(2, len(data_consult.columns), 2):
+            data_consult[days_list[k]] = (
+                    data_consult.iloc[:, i - 1] + ' ' + data_consult.iloc[:, i]
+            )
+            k += 1
+        data_consult = data_consult.iloc[:, [0, 13, 14, 15, 16, 17, 18]]
+        data_consult['Учителя'] = data_consult['Учителя'].map(lambda x: x.strip())
+        self.DataFrames['gid_consult'] = data_consult
+
+    def creatDataClasses(self):
+        self.DataClasses = {str(i): [] for i in range(1, 12)}
+        for elem in list(self.DataFrames['gid'].columns)[4:]:
+            chek_tmp = elem[::]
+            for symd in "йцукенгшщзхъфывапролджэячсмитьбю":
+                chek_tmp = chek_tmp.replace(symd, '')
+            for i in self.DataClasses:
+                if i == chek_tmp and all(['дни' not in elem, 'время' not in elem, 'уроки' not in elem]):
+                    self.DataClasses[i].append(('/getTimeTable/' + elem, elem))
+
+    def get_Data(self, gid=False, gid_consult=False) -> pd.DataFrame:
+        if gid:
+            return self.DataFrames['gid']
+        if gid_consult:
+            return self.DataFrames['gid_consult']
+        return pd.DataFrame()
+
+    def get_classes(self) -> dict:
+        return self.DataClasses
+
+    def geter(self) -> dict:
+        return {
+            'SPREADSHEET_UPL': self._SPREADSHEET_UPL,
+            'id_gid': self._id_gid,
+            'id_gid_consult': self._id_gid_consult,
+            'path_service_account': self._path_service_account,
+            'Client': str(self._gc),
+            'Spreadsheet': str(self._sh),
+            'DataFrame': [self.DataFrames[i].values.tolist() for i in self.DataFrames],
+            'DataClasses': self.DataClasses
+        }
+
+
+# tw = TableWorker(
+#     url='https://docs.google.com/spreadsheets/d/1BCVo9l3744Br5jMmqCEgCryUiCoqOt4H2XiR9ojEYhY/edit#gid=0',
+#     id_gid=[0, 1204865440, 1647332919],
+#     id_gid_consult=[235248533, 2109548677],
+#     path_service_account='TimeTable_serviceAcc.json'
+# )
+# tw.creatData()
+# print(tw.get_Data(gid=True).columns)
+# print(tw.get_Data(gid_consult=True).iloc[:18, :3])
+# tw.creatData()
+# print(tw.get_Data(gid_consult=True).iloc[:18, :3])
---
+++ api/api.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request, jsonify
+from CreatTables import TableWorker
+
+
+application = Flask(__name__, template_folder='templates')
+tw = TableWorker(
+    url='YOUR_URL',
+    id_gid=[YOUR_ID],
+    id_gid_consult=[YOUR_ID],
+    path_service_account='TimeTable_serviceAcc.json'
+)
+tw.creatData()
+tw.creatDataClasses()
+
+
+@application.route('/application/TableWorker/v1')
+def application_TableWorker_v1():
+    return jsonify(tw.geter())
+
+
+@application.route('/')
+def homePage() -> str:
+    return render_template('home.html')
+
+
+@application.route('/time_table_choose_class')
+def time_table_choose_class() -> str:
+    return render_template('time_table.html', classes_data=tw.get_classes())
+
+
+@application.route('/consalt_choose_teacher')
+def consalt_choose_teacher() -> str:
+    return render_template('consalt.html')
+
+
+@application.route('/getTimeTable/<class_>')
+def getTimeTableClass(class_: str) -> str:
+    class_ = class_.lower().replace(' ', '')
+    data = tw.get_Data(gid=True)[['Дни', 'Уроки', 'Время', class_]]
+    data = [i for i in
+            zip(
+                data['Дни'].to_list(),
+                data['Уроки'].to_list(),
+                data['Время'].to_list(),
+                data[class_].to_list()
+            )
+            ]
+    return render_template(
+        'time_table_for_class.html',
+        dataLessons=data,
+        class_=class_
+    )
+
+
+@application.route('/getTimeTableConsult/<teacher>')
+def getTimeTableConsult(teacher: str) -> str:
+    data = tw.get_Data(gid_consult=True)
+    res_data = data[data['Учителя'] == teacher]
+    dataConsultTeacher = [[i] + [' '.join(res_data[i].to_list()[0].split(' ')[:3])] +
+                          [res_data[i].to_list()[0].split(' ')[-1]] for i in res_data.columns[1:]]
+    return render_template(
+        'consalt_TimeTable.html',
+        teacher=teacher,
+        dataConsultTeacher=dataConsultTeacher
+    )
+
+
+@application.route('/update_time_table')
+def updateTimeTable():
+    tw.creatData()
+    return homePage()
+
+
+@application.route('/AdminForm', methods=['POST'])
+def adminForm():
+    passwordAdmin = request.form['name']
+    if passwordAdmin == 'Admin01091903Licei':
+        return updateTimeTable()
+    return homePage()
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
